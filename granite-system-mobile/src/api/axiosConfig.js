aa71b143a729b16a848bf0e8b3df2e5347627cb7
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-04-13 13:31:50 +0530

    Update axiosConfig.js

--- granite-system-mobile/src/api/axiosConfig.js
+++ granite-system-mobile/src/api/axiosConfig.js
@@ -1,10 +1,40 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
+// Local backend (PORT from granite-system-backend) without ngrok.
+// In Expo Go, use the dev machine LAN host from hostUri (e.g. 192.168.1.8).
+const hostUri =
+    Constants.expoConfig?.hostUri ||
+    Constants.manifest2?.extra?.expoClient?.hostUri ||
+    '';
+const devHost = hostUri ? hostUri.split(':')[0] : '';
 
-const BASE_URL = 'http://192.168.1.8:5000/api'; 
+export const SERVER_URL = devHost
+    ? `http://${devHost}:5000`
+    : Platform.OS === 'android'
+        ? 'http://10.0.2.2:5000'
+        : 'http://localhost:5000';
+
+const BASE_URL = `${SERVER_URL}/api`;
 
 const api = axios.create({
     baseURL: BASE_URL,
 });
+
+// Add a request interceptor
+api.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('userToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
