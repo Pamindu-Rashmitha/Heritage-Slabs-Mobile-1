a9b9417dda7844ab2cd85aff7d98de287231f1c4
Piumra Prathiban <piumrawork@gmail.com>
2026-04-18 06:52:29 +0530

    updated server.js to include purchase order part

--- granite-system-backend/server.js
+++ granite-system-backend/server.js
@@ -17,6 +17,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/purchase-orders', require('./routes/purchaseOrderRoutes'));
 
 
 
